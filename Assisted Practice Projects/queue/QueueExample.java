fd48eebdee1eabb1f635fffa71a0f89cc122490b
unknown <pichika.ganapathi@Mphasis.com>
2022-02-08 10:10:12 +0530

    Assisted Projects

---
+++ Assisted Practice Projects/queue/QueueExample.java
@@ -0,0 +1,20 @@
+package queue;
+import java.util.*;
+public class QueueExample {
+	public static void main(String[] args) 
+	{
+	        		Queue<String> locationsQueue = new LinkedList<>();
+	                locationsQueue.add("Hyderabad");
+	        		locationsQueue.add("Mumbai");
+	        		locationsQueue.add("Chennai");
+	        		locationsQueue.add("Allahabad");
+	        		locationsQueue.add("Visakhapatnam");
+	System.out.println("Queue is : " + locationsQueue);
+	        		System.out.println("Head of Queue : " + locationsQueue.peek());
+	        		locationsQueue.remove();
+	        		System.out.println("After removing Head of Queue : " + locationsQueue);
+	        		System.out.println("Size of Queue : " + locationsQueue.size());
+	    	}
+
+
+}
---
+++ Assisted Practice Projects/stack/com/Stack.java
@@ -0,0 +1,5 @@
+package stack.com;
+
+public class Stack {
+
+}
